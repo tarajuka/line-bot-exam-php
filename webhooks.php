54401a8fca10c9a11165926ae10d394e12b32fa2
tarajuka <44927895+tarajuka@users.noreply.github.com>
2018-11-11 03:42:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'MsJ5SCfUSsthzGkTJveR9VSCXy/Vz0gcbuTFgLpI6HbuNprncudjsSbf6DZYEmBEA8Z0ttE2sA3ndfyOWOYHxkOl/Gy7oLk48qt5RshaKocHAXhxPTejtwR+3YrBZkOef4Rcdy9y4xWTgqWfiFLATQdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
